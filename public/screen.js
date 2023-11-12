a7ae5875b471186e4ae5bca3fec66f2756c158d2
ayasemashita <ayase6334@gmail.com>
2023-11-12 16:57:55 +0900

    角度を座標に変換

--- public/screen.js
+++ public/screen.js
@@ -14,38 +14,114 @@ setSocketEventListener('updatePointer', (data)=>{
     const alpha = data.alpha;
     const beta = data.beta;
     const gamma = data.gamma;
-    const p1 = angle2position(alpha);
+    const p = point.getPosition(data);
     
     document.getElementById("output").innerText = `alpha:${alpha}\nbeta:${beta}\ngamma:${gamma}\np1:${p1}`;
-    pointLight.style.left = ((-p1+1)/2 * 100) + '%';
+    pointLight.style.left = ((-p.x+1)/2 * 100) + '%';
+    pointLight.style.top = ((-p.y+1)/2 * 100) + '%';
 
 });
 
-const center = {
-    alpha:0,
-    beta:0,
-    gamma:0,
-}
+
 setSocketEventListener('init position', (data)=>{
-    center.alpha = data.alpha;
-    center.beta = data.beta;
-    center.gamma = data.gamma;
-    document.getElementById("outputInit").innerText = JSON.stringify(center);
+    point.center = data;
+    document.getElementById("outputInit").innerText = JSON.stringify(point.center);
 
 });
 
-function angle2position(alpha){
-    let delta = alpha - center.alpha;
-    if(delta>180){//鋭角、負
-        delta -= 360;
-    }
-    const max = 45;
-    if(delta > max){
-        delta = 45;
-    }else if(delta < -45){
-        delta = -45;
+class Point{
+    constructor(){
+        this.center = {
+            alpha:0,
+            beta:0,
+            gamma:0,
+        }
+        this.left = {
+            alpha:30,
+            beta:0,
+            gamma:0,
+        }
+        this.right = {
+            alpha:330,
+            beta:0,
+            gamma:0,
+        }
+        this.top = {
+            alpha:0,
+            beta:30,
+            gamma:0,
+        }
+        this.bottom = {
+            alpha:0,
+            beta:-30,
+            gamma:0,
+        }
     }
 
-    return delta/max;
+    getPosition(v){
+        const a = oriSubAbs(this.left , this.center);
+        const b = oriSubAbs(this.center , this.right);
+        const theta = oriMul(oriSub(b, a), 0.5);
+        const zeta = oriAdd(oriSub(v, this.center),theta);
+        const l = oriAdd(theta, a);
+        const r = oriSub(theta, b);
+        //console.log(a,b,theta,zeta,l,r);
+
+        const x = angle2liner(zeta.alpha, l.alpha, r.alpha);
+        const y = angle2liner(zeta.beta, l.beta, r.beta);
+
+        return {x, y};
+    }
+}
+const point = new Point();
+
+function angle2liner(z, l, r){
+    const t = Math.tan(deg2rad(z));
+    const t_max = Math.tan(deg2rad(l));
+    const t_min = Math.tan(deg2rad(r));
+
+    return t/(t_max - t_min);
+}
+
+function deg2rad(deg){
+    return (deg * Math.PI) / 180;
+}
+
+function oriMul(a, k){
+    return {
+        alpha: a.alpha * k,
+        beta: a.beta * k,
+        gamma: a.gamma,
+    }
+}
+
+function oriSub(a,b){
+    return {
+        alpha: a.alpha - b.alpha, 
+        beta: a.beta - b.beta,
+        gamma: a.gamma - b.gamma,
+    }
+    //ans.alpha = modulo(ans.alpha, 360);
+}
+
+function oriSubAbs(a,b){
+    const v = oriSub(a,b);
+    v.alpha = modulo(v.alpha, 360);
+    v.beta = modulo(v.beta, 360);
+    v.gamma = modulo(v.gamma, 360);
+    return v;
+}
+
+function oriAdd(a,b){
+    return {
+        alpha: a.alpha + b.alpha, 
+        beta: a.beta + b.beta,
+        gamma: a.gamma + b.gamma,
+    }
+    //ans.alpha = modulo(ans.alpha, 360);
+}
+
+function modulo(a, n){
+    return ((a % n ) + n ) % n;
 }
 
