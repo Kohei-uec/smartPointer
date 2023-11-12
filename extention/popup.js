20e2f65b45aa2bdf37798cd49f22f7a1f62a8043
ayasemashita <ayase6334@gmail.com>
2023-11-13 00:27:59 +0900

    update: ectension

--- extention/popup.js
+++ extention/popup.js
@@ -1,15 +1,32 @@
 
 let buttonElement = document.getElementById('button');
 
-buttonElement.addEventListener('click', (event) => {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        console.log(tabs);
-        chrome.tabs.sendMessage(tabs[0].id, {
-            command: "start"
-        },
-        function(msg) {
-            console.log("result message:", msg);
-            document.getElementById('outputId').innerText = 'id:' + msg;
-        });
-    }); 
+buttonElement.addEventListener('click', async (event) => {
+    getCurrentTab((tab)=>{
+        console.log(tab);
+        chrome.tabs.sendMessage(tab.id, {command: "start", tabId:tab.id},(e)=>showId(e));
+    });
 });
+
+chrome.runtime.onMessage.addListener(async function(msg, sender, sendResponse) {
+    console.log('reserved', msg);
+    if (msg.command && (msg.command == "connected")) {
+        showId('id:' + msg.id);
+        sendResponse();
+    }
+});
+
+const showId = function(msg) {
+    console.log("result message:", msg);
+    document.getElementById('outputId').innerText = msg;
+}
+
+function getCurrentTab(callback) {
+    let queryOptions = { active: true, lastFocusedWindow: true };
+    chrome.tabs.query(queryOptions, ([tab]) => {
+        if (chrome.runtime.lastError)
+        console.error(chrome.runtime.lastError);
+        // `tab` will either be a `tabs.Tab` instance or `undefined`.
+        callback(tab);
+    });
+}
